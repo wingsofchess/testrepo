d08fc5c235639a5889601bf513e345afb5f652ea
wingsofchess <41267179+wingsofchess@users.noreply.github.com>
2024-07-26 11:25:05 +0300

    Create globalHelper.groovy

---
+++ src/com/customfolder/vars/globalHelper.groovy
@@ -0,0 +1,14 @@
+def say_hi() {
+    println("say_hi method")
+    println("Hello World!")
+}
+
+def say_bye() {
+    println("say_bye method")
+    println("No more fun... Bye!")
+}
+
+def do_something() {
+    println("do_something method")
+    println("Will not do. On break.")
+}
